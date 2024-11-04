bb7ac197d2228e051f5a62bb80aefe6a65c8f9e3
Kyla Henry <kylachenry15@gmail.com>
2024-11-04 01:21:40 -0500

    fixing tabs

--- InkyApp/app/(tabs)/write.tsx
+++ InkyApp/app/(tabs)/write.tsx
@@ -4,12 +4,12 @@ import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
 import React from 'react'
 
-const Library = () => {
+const Write = () => {
     return (
         <View>
-            <text>Library</text>
+            <text>Write</text>
         </View>
     )
 }
 
-export default Library
+export default Write
