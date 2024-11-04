1f92f9423a7c42492527fbd888e168fe74e03d2d
Kyla Henry <kylachenry15@gmail.com>
2024-11-04 10:57:43 -0500

    testing tabs again w/ working homepage

--- InkyApp/.expo/types/router.d.ts
+++ InkyApp/.expo/types/router.d.ts
@@ -6,7 +6,7 @@ export * from 'expo-router';
 declare module 'expo-router' {
   export namespace ExpoRouter {
     export interface __routes<T extends string = string> extends Record<string, unknown> {
-      StaticRoutes: `/` | `/(tabs)` | `/(tabs)/` | `/(tabs)/explore` | `/(tabs)\library` | `/(tabs)\search` | `/_sitemap` | `/explore`;
+      StaticRoutes: `/` | `/(tabs)` | `/(tabs)/` | `/(tabs)/library` | `/(tabs)/search` | `/(tabs)\notifications` | `/(tabs)\userprofile` | `/(tabs)\write` | `/_sitemap` | `/library` | `/search`;
       DynamicRoutes: never;
       DynamicRouteTemplate: never;
     }
